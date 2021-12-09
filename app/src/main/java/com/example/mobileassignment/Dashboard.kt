d13939ec734a624fa3da327f2d7ccafdb5ba0bb4
JSKY <jasonsor520@gmail.com>
2021-12-09 19:18:25 +0800

    Add Quota

--- app/src/main/java/com/example/mobileassignment/Dashboard.kt
+++ app/src/main/java/com/example/mobileassignment/Dashboard.kt
@@ -1,9 +1,6 @@
 package com.example.mobileassignment
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,19 +13,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileassignment.databinding.FragmentDashboardBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import android.os.Looper.getMainLooper
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 class Dashboard : Fragment() {
 
     private lateinit var binding : FragmentDashboardBinding
     private lateinit var manager : RecyclerView.LayoutManager
-    private var rack : MutableList<Rack> = mutableListOf()
+    private var rackData : MutableList<Rack> = mutableListOf()
     private lateinit var viewModelData: ViewModelData
     private val db = Firebase.firestore
-    private var someHandler: Handler? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,22 +33,23 @@ class Dashboard : Fragment() {
 
         manager = LinearLayoutManager(requireContext())
 
+
         readMaterial(object : FirestoreCallback {
             override fun onCallback(rack : MutableList<Rack>) {
-                viewModelData.rack = rack
-                recyclerView()
+                //viewModelData.rack = rack
+                //recyclerView()
+
+                getUsedQuota(object : GetUsedQuota {
+                    override fun onCallback(rack : Rack) {
+                        viewModelData.rack = rackData
+                        recyclerView()
+                    }
+                })
+
             }
         })
 
-        // set up date in dashbaord
-        someHandler = Handler(getMainLooper())
-        someHandler!!.postDelayed(object : Runnable {
-            @SuppressLint("SimpleDateFormat")
-            override fun run() {
-                binding.date.text = SimpleDateFormat("(EEE) dd MMM, yyyy      hh:mm:ss a").format(Date())
-                someHandler!!.postDelayed(this, 1000)
-            }
-        }, 10)
+
 
 
         return binding.root
@@ -65,32 +59,46 @@ class Dashboard : Fragment() {
         fun onCallback(rack: MutableList<Rack>)
     }
 
+    private interface GetUsedQuota{
+        fun onCallback(rack: Rack)
+    }
+
     private fun readMaterial(firebaseCallback : FirestoreCallback){
 
         db.collection("Rack").get().addOnSuccessListener { result ->
             for (document in result) {
-                rack.add(Rack(document.data["name"].toString(),document.data["quota"].toString(),document.data["description"].toString()))
+                rackData.add(Rack(document.data["name"].toString(),document.data["quota"].toString(),document.data["description"].toString(),"0"))
             }
-            //Log.w("failedAttempt", "yyyyyyyyyyyyyyyyyyyy")
-            firebaseCallback.onCallback(rack)
+
+            firebaseCallback.onCallback(rackData)
         }
             .addOnFailureListener { exception ->
 
                 Log.w("failedAttempt", "Error getting documents.", exception)
         }
 
-
     }
+
+    private fun getUsedQuota(usedQuota : GetUsedQuota){
+
+        for(rack in rackData){
+            db.collection("Rack").document(rack.rackName).collection("Materials").get().addOnSuccessListener {
+
+                rack.usedQuota =  it.size().toString()
+
+                usedQuota.onCallback(rack)
+            }.addOnFailureListener { exception ->
+
+                Log.w("failedAttempt", "Error getting documents.", exception)
+            }
+        }
+    }
+
 
     private fun recyclerView(){
         binding.materialrecycleView.layoutManager = manager
-        binding.materialrecycleView.adapter = RecyclerViewAdapter(rack)
-    }
+        binding.materialrecycleView.adapter = RecyclerViewAdapter(viewModelData.rack)
 
-    override fun onDestroy() {
-        super.onDestroy()
-        someHandler?.removeCallbacksAndMessages(null)
-        someHandler = null
     }
 
 }
