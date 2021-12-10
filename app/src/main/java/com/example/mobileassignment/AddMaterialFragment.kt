b0b235469d43ed59b80992fe8f302a2e09d82a59
JSKY <jasonsor520@gmail.com>
2021-12-10 17:37:03 +0800

    Add

--- app/src/main/java/com/example/mobileassignment/AddMaterialFragment.kt
+++ app/src/main/java/com/example/mobileassignment/AddMaterialFragment.kt
@@ -9,14 +9,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.mobileassignment.databinding.FragmentAddMaterialBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONException
+import org.json.JSONObject
 
 class AddMaterialFragment : Fragment() {
 
     private lateinit var binding: FragmentAddMaterialBinding
+    private lateinit var scannerIntegrator: IntentIntegrator
+    private lateinit var qrLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private lateinit var viewModelData: ViewModelData
     private var db = Firebase.firestore
 
     override fun onCreateView(
@@ -27,12 +37,19 @@ class AddMaterialFragment : Fragment() {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_add_material, container, false)
 
+        //Create ViewModel (Owner : this activity)
+        viewModelData = ViewModelProvider(this).get(ViewModelData::class.java)
+
+
         val serial = binding.serialInput.text
         val part = binding.partInput.text
         val qty = binding.quantityInput.text
         val status = binding.statusInput.text
         val rackInDate = binding.dateInput.text
         val emp = binding.receivedInput.text
+
+        //Set Qr Code Scanner
+        setupRequiredSetting()
 
         binding.addButton.setOnClickListener {
             uploadData(serial, part, qty, status, rackInDate, emp)
@@ -47,6 +64,7 @@ class AddMaterialFragment : Fragment() {
 
         binding.qrButton.setOnClickListener{
 
+            permissionLauncher.launch(android.Manifest.permission.CAMERA)
         }
 
         return binding.root
@@ -86,6 +104,97 @@ class AddMaterialFragment : Fragment() {
         rackInDate?.clear()
         emp?.clear()
     }
+
+    private fun setDataToView(){
+
+        binding.serialInput.setText(viewModelData.serialNo)
+        binding.partInput.setText(viewModelData.partNo)
+        binding.quantityInput.setText(viewModelData.quantity)
+
+    }
+
+    private fun setupRequiredSetting(){
+
+        //Set up QR Code Scanner
+        setupMyQrScanner()
+
+        //Set up permission
+        setupPermissionLauncher()
+
+        //Set up Qr Launcher
+        setupQrLauncher()
+
+    }
+
+    private fun setupMyQrScanner(){
+
+        //IntentIntegrator(this) for activity
+
+        val promptText = "Please Scan the OR Code"
+
+        scannerIntegrator = IntentIntegrator.forSupportFragment(this)
+        scannerIntegrator.setOrientationLocked(true)
+        scannerIntegrator.setPrompt(promptText)
+
+        //0 for back camera
+        //1 for front camera
+        scannerIntegrator.setCameraId(0)
+        scannerIntegrator.setBeepEnabled(true)
+
+
+    }
+
+    private fun setupQrLauncher(){
+
+        qrLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+            if (it.resultCode == Activity.RESULT_OK) {
+                val result = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
+                if (result != null) {
+                    if (result.contents == null) {
+                        Toast.makeText(
+                            activity,
+                            "InCorrect Format!!! Please Try Again",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        try {
+                            viewModelData.setValue(JSONObject(result.contents))
+                        } catch (e: JSONException) {
+                            e.printStackTrace()
+                            Toast.makeText(
+                                activity,
+                                "InCorrect Format!!! Please Try Again",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(activity, "InCorrect Format!!! Please Try Again", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+            //Navigate to Temp Fragment to Show the data
+            setDataToView()
+
+        }
+    }
+
+    private fun setupPermissionLauncher(){
+
+        //Get Permission
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+
+            if(it){
+                qrLauncher.launch(scannerIntegrator.createScanIntent())
+                Toast.makeText(activity, "Camera Access Granted !!", Toast.LENGTH_LONG).show()
+
+            }else{
+                Toast.makeText(activity, "Please Accept Camera Permission !", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 
 }
 
